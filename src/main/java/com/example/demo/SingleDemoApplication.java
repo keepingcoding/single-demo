92a030fbcdeefbe4d4201e862b7aed0c809a3269
zzs <46862869+keepingcoding@users.noreply.github.com>
2019-10-23 00:25:42 +0800

    log...

--- src/main/java/com/example/demo/SingleDemoApplication.java
+++ src/main/java/com/example/demo/SingleDemoApplication.java
@@ -18,8 +18,10 @@ import org.springframework.context.annotation.PropertySource;
 @ImportResource(locations = {"classpath*:applicationContext.xml"})
 @PropertySource(value = {"classpath:config.properties"}, ignoreResourceNotFound = true)
 public class SingleDemoApplication {
-
     public static void main(String[] args) {
+        //开启log日志彩色显示
+        System.setProperty("log4j.skipJansi", "false");
+
         SpringApplication.run(SingleDemoApplication.class, args);
     }
 }
