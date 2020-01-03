801244885603b7442ced85a9c9346e9affffd682
zzs <46862869+keepingcoding@users.noreply.github.com>
2020-01-03 14:51:54 +0800

    add: websocket

---
+++ src/main/java/com/example/demo/socket/BroadcastController.java
@@ -0,0 +1,39 @@
+package com.example.demo.socket;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
+
+/**
+ * @author zzs
+ * @date 2020/1/2 15:27
+ */
+@Slf4j
+@Controller
+public class BroadcastController {
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @MessageMapping("/receive")
+    @SendTo("/topic/getResponse")
+    public String broadcast(@RequestParam String name) {
+        log.info("receive a message = {}", name);
+        return "success";
+    }
+
+    @RequestMapping("/sendToPage")
+    @ResponseBody
+    public void send() {
+        this.simpMessagingTemplate.convertAndSend("/topic/getResponse", "时间：" + new Date());
+    }
+
+}
